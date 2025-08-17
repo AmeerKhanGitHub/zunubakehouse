e0bee52260ea928d6d104788c04c4af4f3340ac3
AmeerKhanGitHub <ameerkhan.mail@gmail.com>
2025-08-17 15:39:25 +0100

    add changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/zunubakehouse/", // Update if your repo name differs
+//  base: "/zunubakehouse/", // Update if your repo name differs
   plugins: [react()],
 });
